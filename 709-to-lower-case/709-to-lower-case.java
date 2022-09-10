3b5c5fc0e15a53abf5b9039c68d80f7682d3d4c3
Lakshit Jain <75556842+INFIZEUS@users.noreply.github.com>
2022-09-10 18:47:04 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (91.96%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.java
+++ 709-to-lower-case/709-to-lower-case.java
@@ -1,6 +1,8 @@
 class Solution {
     public String toLowerCase(String s) {
-        String str = s.toLowerCase();
-        return str.toString();
+        // String str = s.toLowerCase();
+        // return str.toString();
+        
+        return s.toLowerCase().toString();
     }
 }
